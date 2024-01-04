b21b114cc215ec854d709ad0ca3992f8304fb005
minkguel <146935836+minkguel@users.noreply.github.com>
2024-01-04 18:58:12 +0100

    sd

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -17,23 +17,20 @@ public class Enemy_AI : MonoBehaviour
 
     void Update()
     {
-        
         //Gets distance between player object and enemy ai
         if(player != null)
         {
             distance = Vector2.Distance(transform.position, player.transform.position);
-            Vector2 direction = player.transform.position - transform.position;         // maybe flip it?
+            Vector2 direction = player.transform.position - transform.position;
             direction.Normalize();
             angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         }
         
-
         if (distance < distanceBetween)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
         
-
     }
 }

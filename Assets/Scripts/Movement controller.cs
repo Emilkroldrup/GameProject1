dce751ffde3468c3463a27108350560261453d50
Emilkr <EmilKroldrup@outlook.com>
2024-01-04 12:50:40 +0100

    Movement v3

--- Assets/Scripts/Movement controller.cs
+++ Assets/Scripts/Movement controller.cs
@@ -2,35 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movementcontroller : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     private Rigidbody2D rb;
-    private Vector2 moveDirection;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    // Update is called once per frame
     void Update()
     {
-        ProcessInputs();
-    }
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-    private void FixedUpdate()
-    {
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
-    void ProcessInputs()
+    private void Move()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
     }
 
-    void Move()
+    private void Jump()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    void OnDrawGizmos()
+    {
+        // This will draw a circle in the editor to visualize the ground check area
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }

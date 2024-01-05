e0974db902c1efe0a7676bbb2d71ed9d659c9e9f
Mads505m <onlinebigchungus@gmail.com>
2024-01-05 06:21:44 +0100

    Fix 2.0 skullEmoji

--- Assets/Scripts/Enemy_AI.cs
+++ Assets/Scripts/Enemy_AI.cs
@@ -24,13 +24,15 @@ public class Enemy_AI : MonoBehaviour
             Vector2 direction = player.transform.position - transform.position;
             direction.Normalize();
             angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            if (distance < distanceBetween)
+            {
+                transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            }
         }
         
-        if (distance < distanceBetween)
-        {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-        }
+       
         
     }
 }

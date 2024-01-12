66304d505ff67af49b1f16f6a8c425706ba138d2
Mads505m <onlinebigchungus@gmail.com>
2024-01-12 09:16:57 +0100

    Update Conditions.cs

--- Assets/Scripts/Conditions.cs
+++ Assets/Scripts/Conditions.cs
@@ -11,6 +11,7 @@ public class Conditions : MonoBehaviour
     public GameObject Playercanvas;
     private bool hasInitialized = false;
     public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
